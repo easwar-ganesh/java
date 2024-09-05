cbcd7062168cfd2ff79f0a5aed6860d4971e01d4
EaswarGanesh <151706281+easwar-ganesh@users.noreply.github.com>
2024-09-05 12:45:02 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Student
+{
+	public static void main(String[] args) 
+	{
+	    String rollno;
+        double avg;
+		rollno="23K61A05G2";
+		avg=95.6;
+		System.out.println("Rollno is "+rollno);
+		System.out.println("Average is"+avg);
+	}
+}
