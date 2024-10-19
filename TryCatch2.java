c898de4d2146e7bc9b3c43222eafc5e6e85cdb39
EaswarGanesh <151706281+easwar-ganesh@users.noreply.github.com>
2024-10-19 12:00:14 +0530

    TryCatch2.java

---
+++ TryCatch2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class TryCatch2{
+    public static void main(String[] args) {
+        int a=5,b=0,c;
+        try
+        {
+        c=a/b;
+        System.out.println("value of c is : "+c);
+        
+        }
+        catch(Exception e)
+        {
+            System.out.println("Exception caught");
+        }
+        }
+    
+}
